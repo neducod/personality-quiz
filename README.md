451b65eb78f210af84d301861c5a4d6e80562c69
neducod <nedunony16@gmail.com>
2025-09-09 17:56:17 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# personality-quiz
+
+Ongoing Project
---
+++ script.js
@@ -0,0 +1,158 @@
+ /*   const questions = [
+        {
+            question: "What excites you the most about coding?",
+            answers: [
+                {text: "Designing stunning interfaces"}
+            ]
+        }
+    ]
+*/
+
+// let questions;
+
+const questions = [
+    {question: "What excites you the most about coding?", 
+    answers: [
+        {text: "Designing stunning interfaces", type: "A"},
+        {text: "Designing stunning interfaces", type: "B"},
+        {text: "Designing stunning interfaces", type: "C"},
+        {text: "Designing stunning interfaces", type: "D"},
+        // {text: "Designing stunning interfaces",text: "Solving complex algorithms", text: "Building complete projects from scratch", text: "Debugging until everything is perfect"}
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "What’s your ideal coding playlist?", 
+    answers: [
+        {text: "Lo-fi aesthetic vibes", type: "A"},
+        {text: "Silence or white noise", type: "B"},
+        {text: "A mix of everything", type: "C"},
+        {text: "Anything that helps me focus while debugging", type: "D"},
+            ]
+    },
+    {question: "You have an hour to learn something new, what do you pick?", 
+    answers: [
+        {text: "CSS animations", type: "A"},
+        {text: "Advanced algorithms", type: "B"},
+        {text: "A new full-stack framewor", type: "C"},
+        {text: "Regex tricks", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+    {question: "Your favorite part of a website is...", 
+    answers: [
+        {text: "The visuals and animations", type: "A"},
+        {text: "The backend magic", type: "B"},
+        {text: "How the front and back work together", type: "C"},
+        {text: "That everything runs without errors", type: "D"},
+            ]
+    },
+
+]
+
+const score = {A: 0, B: 0, C: 0, D: 0},
+
+userAnswers = [];
+
+
+let currentQuestionIndex = 0;
+score[type]++
+
+showQuestion( questions[currentQuestionIndex] ){
+    
+}
+
+
+/*
+One questions array (objects inside)
+
+One scores object
+
+One answers array (user’s choices)
+
+One result object (personalities) */
